922c20567ec801074152cce022b009d2eba541a9
Zumooo <nicolas.cidoncha@alumnos.upm.es>
2026-05-20 20:46:16 +0200

    scrapper

--- src/main/java/org/inteligentes/AgenteScrapper.java
+++ src/main/java/org/inteligentes/AgenteScrapper.java
@@ -6,19 +6,29 @@ import org.json.JSONObject;
 import java.nio.file.Paths;
 
 public class AgenteScrapper extends Agent {
+        Playwright playwright;
+        Browser browser; // tenemos un browser para buscar sobre las paginas
+        BrowserContext browserContext; // contexto del browser
+
+        // el setup de la AGENTE PERCEPCION (webScrapping)
         protected void setup() {
-            String url = "https://www.pccomponentes.com/amd-ryzen-7-7800x3d-42-ghz-5-ghz";
 
-            try (Playwright playwright = Playwright.create()) {
-                BrowserType.LaunchOptions opciones = new BrowserType.LaunchOptions().setHeadless(true);
-                Browser browser = playwright.chromium().launch(opciones);
-                BrowserContext context = browser.newContext(new Browser.NewContextOptions()
-                        .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
-                );
+            playwright = Playwright.create();
+            BrowserType.LaunchOptions opciones = new BrowserType.LaunchOptions().setHeadless(true);
+            browser = playwright.chromium().launch(opciones);
+            browserContext = browser.newContext(new Browser.NewContextOptions());
 
-                Page page = context.newPage();
-                System.out.println("Navegando a la web...");
-                page.navigate(url);
+            // ejemplos
+            scrapePrecio("https://www.pccomponentes.com/amd-ryzen-7-7800x3d-42-ghz-5-ghz");
+            scrapePrecio("https://www.pccomponentes.com/placa-base-asus-tuf-gaming-b850-plus-wifi");
+
+        }
+        public Double scrapePrecio(String enlace){
+            Page page = browserContext.newPage();
+            Double precio = (double) -1;
+            try{
+                System.out.println("Iniciando la busqueda del precio");
+                page.navigate(enlace);
 
                 System.out.println("Título de la página cargada: " + page.title());
 
@@ -26,15 +36,26 @@ public class AgenteScrapper extends Agent {
 
                 JSONObject json = new JSONObject(jsonText);
 
-                String precio = json
+                String resultado = json
                         .getJSONObject("offers")
                         .getJSONObject("offers")
                         .getString("price");
 
-                System.out.println("Precio: " + precio + " €");
-
-                browser.close();
+                System.out.println("Precio encontrado: " + resultado );
+                precio = Double.parseDouble(resultado);
+            } catch (Exception e) {
+                System.out.println("No se ha encontrado el precio el error es:");
+                e.printStackTrace();
+            } finally {
+                page.close();
             }
+            return precio;
         }
+        protected void takeDown() {
+            if(browserContext != null) browserContext.close();
+            if(browser != null) browser.close();
+            if(playwright != null) playwright.close();
+        }
+
 }
 
