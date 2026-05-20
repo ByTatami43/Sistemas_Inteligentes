e71b8d2a1f277a7fd77400316f1fe19b9fe1f0f5
Ldelillo <ldelillo99@gmail.com>
2026-05-20 21:40:20 +0200

    Actualizacion de Producto

--- src/main/java/org/inteligentes/Producto.java
+++ src/main/java/org/inteligentes/Producto.java
@@ -1,21 +1,37 @@
 package org.inteligentes;
 
+import java.util.ArrayList;
 import java.time.LocalDateTime;
 
 public class Producto {
-    String nombre;
-    String enlace;
-    LocalDateTime fecha;
-    Double precioActual;
-    Double precioMinimo;
-    Double umbral;
-    boolean alerta;
-    public Producto(String nombre, String enlace,Double umbral){
+
+    private class PHistorial {
+        
+        private ArrayList<LocalDateTime> fecha;
+        private ArrayList<Double> precios;
+
+        private PHistorial(){
+            this.fecha = new ArrayList<LocalDateTime>();
+            this.precios = new ArrayList<Double>();
+        }
+    }
+
+    private String nombre;
+    private String enlace;
+    private Double precioMinimo;
+    private Double umbral;
+    private boolean alerta;
+    private PHistorial historial;
+
+    public Producto(String nombre, String enlace, Double umbral) {
         this.nombre = nombre;
         this.enlace = enlace;
         this.umbral = umbral;
+        this.precioMinimo = Double.MAX_VALUE;
         alerta = false;
+        historial = new PHistorial();
     }
+
     public String getNombre() {
         return nombre;
     }
@@ -32,20 +48,28 @@ public class Producto {
         this.enlace = enlace;
     }
 
-    public LocalDateTime getFecha() {
-        return fecha;
+    public ArrayList<LocalDateTime> getFechas() {
+        return new ArrayList<>(historial.fecha);
     }
 
+    //ESTO NO LO VEO NECESARIO SALVO PARA AÑADIR A MANO
     public void setFecha(LocalDateTime fecha) {
-        this.fecha = fecha;
+        historial.fecha.add(fecha);
+    }
+
+    public ArrayList<Double> getPrecios() {
+        return new ArrayList<>(historial.precios);
     }
 
     public Double getPrecioActual() {
-        return precioActual;
+        return historial.precios.isEmpty()?null:historial.precios.get(historial.precios.size() - 1);
     }
 
     public void setPrecioActual(Double precioActual) {
-        this.precioActual = precioActual;
+        setFecha(LocalDateTime.now());
+        if (precioActual < precioMinimo)
+            setPrecioMinimo(precioActual);
+        historial.precios.add(precioActual);
     }
 
     public Double getPrecioMinimo() {
@@ -72,4 +96,12 @@ public class Producto {
         this.alerta = alerta;
     }
 
+
+    //Otra auxiliar para rellenar BBDD
+    public void upProducto(LocalDateTime fecha, Double precio){
+        if (precio < precioMinimo)
+            setPrecioMinimo(precio);
+        historial.precios.add(precio);
+        historial.fecha.add(fecha);
+    }
 }
